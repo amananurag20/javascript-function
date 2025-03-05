fc66b3a60709591f9dd05927ea708afc6f01aae8
aman anurag <amananurag.20@gmail.com>
2025-03-05 13:41:21 +0530

    map filter reduce

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-console.log(a);
+//write a program
 
-var a = 25;
+let arr = [1, 2, 3, 4, 5]; //->[1,4,9,16,25]
+
+let ans = arr.every((element) => {
+  return element <= 5;
+});
+
+console.log(ans);
